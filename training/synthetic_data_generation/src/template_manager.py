57a3c45abc32d0b218158454cba9c7a42d316af4
Adam Skrodzki <adam@everdome.io>
2025-07-09 16:59:27 +0200

    partial fix

--- training/synthetic_data_generation/src/template_manager.py
+++ training/synthetic_data_generation/src/template_manager.py
@@ -21,7 +21,7 @@ class TemplateManager:
             "Generate simple, short (up to 150 words) bed time story easy to understand and follow by 3 years old\n"
             "containing words {word1} {word2} {word3}\n\n"
             "{additional_condition}\n\n"
-            "keep story coherent and gramatically correct"
+            "keep story coherent and gramatically correct, write full content of the story and nothing else (no commentary, title, etc)"
         )
         
         self.story_features = []
